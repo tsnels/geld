c19b542811aece9ca9ec16e251a71c00a434dd8d
tsnels <81826313+tsnels@users.noreply.github.com>
2022-04-26 09:39:30 +0200

    Initial commit

---
+++ src/main/java/be/vdab/geld/GeldApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.geld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeldApplication.class, args);
+    }
+
+}
